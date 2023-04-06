aa45d5e1883b35ab4472ee827ca6f7cd0dcafe54
Vinicius <vinicius.lima.brasil1@gmail.com>
2023-04-06 12:40:00 -0300

    route: new deposit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ const app = express();
 app.use(express.json());
 const customers = [];
 
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        res.status(400).json({ error: "Customer not found" })
+    }
+
+    req.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -27,13 +41,26 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 });
 
-app.get("/statement/:cpf", (req, res) => {
-    const { cpf } = req.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
     return res.json(customer.statement);
 });
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+    const { description, amount } = req.body;
+
+    const { customer } = req;
+
+    const statementOperation = {
+        description,
+        amount,
+        createdAt: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+})
 
 app.listen(3333);
 
