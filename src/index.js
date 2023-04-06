fb7465f090eff7f1c7e87e1b9a535b9e5c0766a2
Vinicius <vinicius.lima.brasil1@gmail.com>
2023-04-06 12:54:23 -0300

    route: client able to withdraw

--- src/index.js
+++ src/index.js
@@ -19,6 +19,19 @@ function verifyIfExistsAccountCPF(req, res, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc = operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
+
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -45,6 +58,7 @@ app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
     const { customer } = req;
     return res.json(customer.statement);
 });
+
 app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
     const { description, amount } = req.body;
 
@@ -60,7 +74,28 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
     customer.statement.push(statementOperation);
 
     return res.status(201).send();
-})
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return res.status(400).json({ error: "insuficient funds" });
+    }
+
+    const statementOperation = {
+        amount,
+        createdAt: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
 
 app.listen(3333);
 
